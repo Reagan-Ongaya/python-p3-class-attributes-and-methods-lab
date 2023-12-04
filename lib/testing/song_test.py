8919dc1d23f9002ae6f83e7b7ff6d330cb05a0a7
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-12-04 07:33:57 +0300

    Assignment

--- lib/testing/song_test.py
+++ lib/testing/song_test.py
@@ -9,6 +9,7 @@ class TestSong:
     Song("Halo", "Beyonce", "Pop")
     Song("Smells Like Teen Spirit", "Nirvana", "Rock")
 
+
     def test_saves_name_artist_genre(self):
         '''instantiates with a name, artist, and genre.'''
         out_of_touch = Song("Out of Touch", "Hall and Oates", "Pop")
